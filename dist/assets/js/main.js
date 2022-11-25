436e9faacddfd5d7e28ec2867e768a4328260dc4
MoazHassain <moazhassain@gmail.com>
2022-11-26 02:59:24 +0600

    component page done

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -216,3 +216,25 @@ document.addEventListener("click", e => {
 
     
 });
+
+
+/* custom range */
+const rangeInputs = document.querySelectorAll('input[type="range"]');
+
+function handleInputChange(e) {
+    let target = e.target
+    if (e.target.type !== 'range') {
+        target = document.getElementById('range')
+        console.log(target)
+    } 
+    const min = target.min
+    const max = target.max
+    const val = target.value
+
+    
+    target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+}
+
+rangeInputs.forEach(input => {
+    input.addEventListener('input', handleInputChange)
+});
